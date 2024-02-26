6821cbc6566818ad84d0b7a58ff4bc35e770822f
f-rxfxel <r.porfirio010@gmail.com>
2024-02-25 22:34:12 -0300

    fix: removing remaining code from testing components

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,10 +1,6 @@
-import AccessibleImage from '../_components/accessible-image';
 
 export default function Teste() {
   return (
-    <AccessibleImage
-      title={'a'}
-      src={'/assets/blog/dynamic-routing/cover.jpg'}
-    />
+    <></>
   );
 }
